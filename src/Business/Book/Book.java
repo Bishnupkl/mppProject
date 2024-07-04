a5567c2a88244ec66db4e1a0072d4abf1c51a7c9
pengleng <jk6666depro@gmail.com>
2024-07-04 14:37:19 -0500

    second commit

--- src/Business/Book/Book.java
+++ src/Business/Book/Book.java
@@ -9,8 +9,12 @@ public class Book {
     private String title;
     private List<BookCopy> bookCopies;
     private List<Author> authors;
-    public Book()
-    {
 
+    public Book(String newIsbn, String newTitle, List<BookCopy> newBookCopies, List<Author> newAuthors) {
+        this.isbn = newIsbn;
+        this.title = newTitle;
+        this.bookCopies = newBookCopies;
+        this.authors = newAuthors;
     }
+
 }
