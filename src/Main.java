29ba0aab983211440c1b6bc167d97e7fe8149985
bishnu <bishnup212@gmail.com>
2024-07-04 17:41:05 -0500

    login working from main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import Business.Person.Administrator;
 import Business.Person.Librarian;
 import Business.Person.User;
+import UI.Login;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -9,18 +11,24 @@ public class Main {
     public static void main(String[] args) {
         addDefaultUser();
 
-        User u = User.login("librarian","123");
+//        User u = User.login("librarian","123");
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Login mf = new Login();
+            }
+        });
     }
 
-    private static void addDefaultUser()
-    {
+    private static void addDefaultUser() {
         User administrator = new User("admin", "123", new ArrayList<>(Arrays.asList(new Administrator())));
 
-        User librarian = new User("librarian","123", new ArrayList<>(Arrays.asList(new Librarian())));
+        User librarian = new User("librarian", "123", new ArrayList<>(Arrays.asList(new Librarian())));
 
 //        HashSet<User> users = new HashSet<>();
 //        users.add(administrator);
 //        users.add(librarian);
+
 
         administrator.addDefaultUser(new ArrayList<>(Arrays.asList(administrator, librarian)));
     }
