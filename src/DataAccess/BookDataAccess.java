1bee0381b61e12b0d1f8feff2f070039894ba9f7
ThezThaw <97180604+ThezThaw@users.noreply.github.com>
2024-07-04 14:29:51 -0500

    Add read method

--- src/DataAccess/BookDataAccess.java
+++ src/DataAccess/BookDataAccess.java
@@ -1,4 +1,11 @@
 package DataAccess;
 
+import Business.Book.Book;
+
 public class BookDataAccess extends FileDataAccess{
+
+    public void readBook()
+    {
+        Book book =  (Book)super.read("book.bin");
+    }
 }
--- src/DataAccess/FileDataAccess.java
+++ src/DataAccess/FileDataAccess.java
@@ -1,4 +1,22 @@
 package DataAccess;
 
-public class FileDataAccess implements DataAccess {
+import java.io.*;
+
+public class FileDataAccess{
+
+
+    public Object read(String fileName) {
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(fileName);
+            return new ObjectInputStream(file).readObject();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
