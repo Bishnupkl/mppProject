24b864ff29973c5834fc98fd6ac01a8260fc7728
pengleng <jk6666depro@gmail.com>
2024-07-04 14:32:41 -0500

    Add book attributes

--- src/Business/Book/Book.java
+++ src/Business/Book/Book.java
@@ -5,6 +5,8 @@ import Business.Person.Author;
 import java.util.List;
 
 public class Book {
+    private String isbn;
+    private String title;
     private List<BookCopy> bookCopies;
     private List<Author> authors;
     public Book()
