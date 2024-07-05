6725b289c8bec6c11ce79af42dd3018ee960f359
pengleng <jk6666depro@gmail.com>
2024-07-04 22:25:54 -0500

    latest

--- src/Business/Book/Book.java
+++ src/Business/Book/Book.java
@@ -19,23 +19,23 @@ public class Book implements Serializable {
     private List<BookCopy> bookCopies = new ArrayList<>();
     private List<Author> authors = new ArrayList<>();
 
-    public Book(String newIsbn, String newTitle, int newBorrowDuration,Author newAuthor) {
+    public Book(String newIsbn, String newTitle, int newBorrowDuration, Author newAuthor) {
         this.isbn = newIsbn;
         this.title = newTitle;
-        this.borrowDuration=newBorrowDuration;
+        this.borrowDuration = newBorrowDuration;
         BookCopy copyOne = new BookCopy(this);
         this.bookCopies.add(copyOne);
         newAuthor.setBook(this);
         this.authors.add(newAuthor);
     }
 
-    public Book(String newIsbn, String newTitle, int newBorrowDuration,List<Author> newAuthors) {
+    public Book(String newIsbn, String newTitle, int newBorrowDuration, List<Author> newAuthors) {
         this.isbn = newIsbn;
         this.title = newTitle;
-        this.borrowDuration=newBorrowDuration;
+        this.borrowDuration = newBorrowDuration;
         BookCopy copyOne = new BookCopy(this);
         this.bookCopies.add(copyOne);
-        for (Author a:newAuthors){
+        for (Author a : newAuthors) {
             a.setBook(this);
         }
         this.authors.addAll(newAuthors);
@@ -45,14 +45,11 @@ public class Book implements Serializable {
         return isbn;
     }
 
-    public static StatusInfoWrapper checkBookExist(String isbn)
-    {
+    public static StatusInfoWrapper checkBookExist(String isbn) {
         Book book = BookDataAccess.readBook(isbn);
-        if(book == null)
-        {
+        if (book == null) {
             return new StatusInfoWrapper(false, null, MessageConstant.BOOK_NOT_FOUND);
-        }
-        else {
+        } else {
             return new StatusInfoWrapper(true, book, null);
         }
     }
@@ -60,34 +57,31 @@ public class Book implements Serializable {
     public static StatusInfoWrapper addBook(Book newBook) {
         StatusInfoWrapper result = checkBookExist(newBook.getIsbn());
         System.out.println(result.getValue());
-        if(result.getStatus() == false)
-        {
+        if (result.getStatus() == false) {
             BookDataAccess.createNewBook(newBook);
-            return new StatusInfoWrapper(true,null, "Create Book Successful");
-        } else{
-            return new StatusInfoWrapper(false,null, "Book already exist in the system");
+            return new StatusInfoWrapper(true, null, "Create Book Successful");
+        } else {
+            return new StatusInfoWrapper(false, null, "Book already exist in the system");
         }
     }
 
     public static StatusInfoWrapper addCopy(String newIsbn) {
         StatusInfoWrapper result = checkBookExist(newIsbn);
         System.out.println(result.getStatus());
-        if(result.getStatus())
-        {
-            Book book = (Book)result.getValue();
+        if (result.getStatus()) {
+            Book book = (Book) result.getValue();
             book.setBookCopies(new BookCopy(book));
             BookDataAccess.createNewBookCopy(book);
-            return new StatusInfoWrapper(true,null,"Add Copy Successful");
+            return new StatusInfoWrapper(true, null, "Add Copy Successful");
         }
-        return new StatusInfoWrapper(true,null,"Cannot add copy");
+        return new StatusInfoWrapper(true, null, "Cannot add copy");
     }
 
-    public static List<BookCopy> getBookCopies(String isbn){
+    public static List<BookCopy> getBookCopies(String isbn) {
 
         StatusInfoWrapper result = checkBookExist(isbn);
-        if(result.getStatus())
-        {
-            return ((Book)result.getValue()).getBookCopies();
+        if (result.getStatus()) {
+            return ((Book) result.getValue()).getBookCopies();
         }
 
         return null;
@@ -97,12 +91,30 @@ public class Book implements Serializable {
 //        Address address= new Address("4th","fairfield","IOWA","52556");
 //        Author author= new Author("John","Cena","641222333",address,"Good Author","Nice Author");
 //        Book.addBook(new Book("How to","13",7,author));
-        Book.addCopy("11");
+//        Book.addCopy("11");
 //        List<BookCopy> copies = Book.getBookCopies("How to");
     }
 
     public void setBookCopies(BookCopy bookCopy) {
         this.bookCopies.add(bookCopy);
+    }
+
+    public StatusInfoWrapper getAvailableCopy(String isbn){
+        Book book = BookDataAccess.readBook(isbn);
+        if(book==null){
+            return new StatusInfoWrapper(false,null,"Book does not exist in system");
+        }else{
+            List<BookCopy> copies=book.getBookCopies();
+            for (BookCopy copy:copies){
+                if(copy.getCheckoutRecord().equals(null)){
+                    return new StatusInfoWrapper(true,copy,"success");
+                } else if (!copy.getCheckoutRecord().getReturnDate().equal(null)) {
+                    return new StatusInfoWrapper(true,copy,"success");
+
+                }
+            }
+            return new StatusInfoWrapper(false,null,"No Copy available");
+        }
     }
 
     public List<BookCopy> getBookCopies() {
--- src/Business/Book/BookCopy.java
+++ src/Business/Book/BookCopy.java
@@ -1,14 +1,26 @@
 package Business.Book;
 
+import Business.Checkout.CheckoutRecord;
+
 import java.io.Serializable;
 
 public class BookCopy implements Serializable {
 
     private String id;
     private Book book;
+
+    private CheckoutRecord checkoutRecord;
     public BookCopy(Book newBook){
         this.id= String.valueOf(Math.random());
         this.book= newBook;
+    }
+
+    public CheckoutRecord getCheckoutRecord() {
+        return checkoutRecord;
+    }
+
+    public void setCheckoutRecord(CheckoutRecord checkoutRecord) {
+        this.checkoutRecord = checkoutRecord;
     }
 
     @Override
