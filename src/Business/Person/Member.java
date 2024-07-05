2984229fe1dbf977d673cad9a45240bf455bd0b4
ThezThaw <97180604+ThezThaw@users.noreply.github.com>
2024-07-04 19:52:05 -0500

    Add addMember function

--- src/Business/Person/Member.java
+++ src/Business/Person/Member.java
@@ -1,13 +1,22 @@
 package Business.Person;
 
 import Business.Checkout.CheckoutRecord;
+import DataAccess.PersonDataAccess;
 
 import java.util.List;
 
 public class Member extends Person {
     private List<CheckoutRecord> checkoutRecords;
-    public Member()
+    private Member(String firstName, String lastName, String telephone, Address address)
     {
-        super("","");
+        super(firstName, lastName, telephone, address);
+    }
+
+    public static Member addMember(String firstName, String lastName, String street, String city, String state, int zip, String telephone)
+    {
+        Address address = new Address(street, city, state, zip);
+        Member member = new Member(firstName, lastName, telephone, address);
+        PersonDataAccess.addPerson(member);
+        return member;
     }
 }
