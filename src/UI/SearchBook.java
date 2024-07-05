6d1d579e7de1234db6d2d218589d6d18cec0b4fb
bishnu <bishnup212@gmail.com>
2024-07-05 08:51:26 -0500

    Adding Book Copy Maintainance

--- src/UI/SearchBook.java
+++ src/UI/SearchBook.java
@@ -1,11 +1,40 @@
 package UI;
 
-import javax.swing.*;
+import Business.Book.Book;
+import Business.Book.BookCopy;
 
-public class SearchBook {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class SearchBook extends  JFrame {
     private JPanel searchBook;
-    private JTextField textField1;
+    private JTextField isnValue;
     private JButton searchButton;
 
+    public SearchBook() {
+        setVisible(true);
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Main Menu");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        setContentPane(searchBook);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                String isbn = isnValue.getText();
+
+                List<BookCopy> bookCopies = Book.getBookCopies(isbn);
+                for (BookCopy copy : bookCopies) {
+                    System.out.println(copy.getId());
+
+                }
+
+            }
+        });
+    }
 
 }
