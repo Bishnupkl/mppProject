5498831db7a924bde4550a5420e3796bae29c02c
ThezThaw <97180604+ThezThaw@users.noreply.github.com>
2024-07-05 19:56:29 -0500

    Zaw latest (#41)
    * Add checkout function - wip
    
    * Add checkout function - wip
    
    * resolve conflict
    
    * Add checkout function - wip
    
    * Bug fix
    
    * refactor name
    
    * Add sequence diagram - wip
    
    * Change label name
    
    * Add usecase diagram
    
    * Add class diagram
    
    * Add addbook sequence diagram
    
    * Add credential properties
    
    * Add helper/person factory
    
    * Update class diagram
    
    * refactor

--- src/Business/Checkout/CheckoutRecordFactory.java
+++ src/Business/Checkout/CheckoutRecordFactory.java
@@ -3,9 +3,9 @@ package Business.Checkout;
 import Business.Book.BookCopy;
 import Business.Person.Member;
 
-public class CheckoutRecordFactory {
+public final class CheckoutRecordFactory {
     private CheckoutRecordFactory(){}
-    public static CheckoutRecord createCheckoutRecord(Member member, BookCopy bookCopy)
+    public static CheckoutRecord generateCheckoutRecord(Member member, BookCopy bookCopy)
     {
         return new CheckoutRecord(member, bookCopy);
     }
